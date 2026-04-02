57960d2aab8645597cc44c132a55b62fcdb6edaf
Md Nouman <noumantannu06@gmail.com>
2026-04-02 18:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function firstWord(s) {
   // your code here
-	 if (s === "") return s;
+  if (s === "") return s;
 
-  firstSpaceIndex = s.indexOf(" ");
+  const firstSpaceIndex = s.indexOf(" ");
 
-  (firstSpaceIndex === -1) return s;
+  if (firstSpaceIndex === -1) return s;
 
-  s.slice(0, firstSpaceIndex);
-	
+  return s.slice(0, firstSpaceIndex);
 }
+
 // Do not change the code below
 
 const s = prompt("Enter String:");
