fc5a32080b762994048a0f9f71e169220933312e
Md Nouman <noumantannu06@gmail.com>
2026-04-02 18:42:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	 if (str === "") return str;
+
+  // Find the index of the first space.
+  const firstSpaceIndex = str.indexOf(" ");
+
+  // If there's no space, return the whole string.
+  if (firstSpaceIndex === -1) return str;
+
+  // Return the substring from start up to (but not including) the first space.
+  return str.slice(0, firstSpaceIndex);
+	
 }
 
 // Do not change the code below
